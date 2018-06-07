4f0c06e98301c99ba051204a3d0f89b85831e117
c10t <c10t@fa.ke>
2018-06-08 01:02:01 +0900

    remove commentouted imports

--- frontend/src/components/Sandbox.tsx
+++ frontend/src/components/Sandbox.tsx
@@ -1,10 +1,5 @@
 import * as React from 'react';
 
-// import Button from '@material-ui/core/Button';
-// import Card from '@material-ui/core/Card';
-// import CardActions from '@material-ui/core/CardActions';
-// import CardContent from '@material-ui/core/CardContent';
-
 import  { withStyles, WithStyles } from '@material-ui/core';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Paper from '@material-ui/core/Paper';
