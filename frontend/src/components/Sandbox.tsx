c80bdcfcb618d6f99d2daa2383430ee28d5405fb
c10t <c10t@fa.ke>
2018-06-08 01:01:25 +0900

    small refactor: change import text

--- frontend/src/components/Sandbox.tsx
+++ frontend/src/components/Sandbox.tsx
@@ -5,14 +5,18 @@ import * as React from 'react';
 // import CardActions from '@material-ui/core/CardActions';
 // import CardContent from '@material-ui/core/CardContent';
 
-import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core'
 import  { withStyles, WithStyles } from '@material-ui/core';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import Paper from '@material-ui/core/Paper';
-import { Theme } from '@material-ui/core/styles';
-import { createStyles } from '@material-ui/core/styles';
+import { createStyles, Theme } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
+
 import Search from '@material-ui/icons/Search';
 
 const styles = (theme: Theme) => createStyles({
